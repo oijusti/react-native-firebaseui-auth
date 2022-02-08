7f901c44e174d4f6d47b51730b84335876158ab1
Oscar Justi <oijusti@gmail.com>
2022-02-07 20:35:53 -0500

    Update test (#45)

--- example/__tests__/App-test.js
+++ example/__tests__/App-test.js
@@ -9,6 +9,8 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
